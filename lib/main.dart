2cec19f031c781f1ed041906678fd2453bdea135
lengyue524 <lengyue524@gmai.com>
2024-05-25 22:19:16 +0800

    fix: 修复dart 警告

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
             ),
             localizationsDelegates: AppLocalizations.localizationsDelegates,
             supportedLocales: AppLocalizations.supportedLocales,
-            locale: Locale((state as AppLaunchComplated).local.name),
+            locale: Locale((state).local.name),
             home: MultiBlocProvider(
               providers: [
                 BlocProvider<ConfigBloc>(
